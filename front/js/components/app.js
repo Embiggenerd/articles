f27d19dc72cd87d39cb4b6285d00f4ba2f5610f0
Embiggenerd <embiggenerd@gmail.com>
2025-06-18 17:12:57 -0700

    working on render function

---
+++ front/js/components/app.js
@@ -0,0 +1,36 @@
+const appComponent = {
+    renderer: null,
+    mediaService: null,
+    messageService: null,
+    async init(renderer) {
+        try {
+            this.renderer = renderer()
+            
+        } catch (e) {
+            this.handleError(e)
+        }
+    },
+
+    
+    handleError: function (error) {
+        console.error(error)
+        // const message = {
+        //     text: error,
+        //     from_user_name: 'ADMIN (to you)',
+        // }
+        // if (error.data) {
+        //     message.text = error.data
+        // }
+        // if (error instanceof Error) {
+        //     message.text = error.message
+        // }
+        // if (error.text) {
+        //     message.text = error.text
+        // }
+        // this.renderer?.chatLog.addMessage(message)
+    },
+
+    
+}
+
+export default appComponent
