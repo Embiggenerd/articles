05173f2214fbcb020e28142bc2e4af976617d555
Igor Atakhanov <embiggenerd@gmail.com>
2025-06-24 12:01:24 -0700

    creating local frontend system

--- back/articles.go
+++ back/articles.go
@@ -3,47 +3,111 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
-	twitterLogin "github.com/dghubble/gologin/v2/twitter"
-	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
 )
+
+// twitterLogin "github.com/dghubble/gologin/v2/twitter"
+// "github.com/dghubble/oauth1"
 
 // Define your Consumer Key and Consumer Secret
-const (
-	ConsumerKey    = "YOUR_CONSUMER_KEY"
-	ConsumerSecret = "YOUR_CONSUMER_SECRET"
-)
+// const (
+// 	ConsumerKey    = "X_CONSUMER_KEY"
+// 	ConsumerSecret = "X_CONSUMER_SECRET"
+// )
 
-func main() {
-	// OAuth1 Config for Twitter
-	oauth1Config := &oauth1.Config{
-		ConsumerKey:    ConsumerKey,
-		ConsumerSecret: ConsumerSecret,
-		CallbackURL:    "http://localhost:8080/twitter/callback",
-		Endpoint:       oauth1.TwitterEndpoint,
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	// ctx := r.Context()
+	// reqID, _ := utils.ExposeContextMetadata(ctx).Get("requestID")
+	// if r.URL.Path != "/" {
+	// 	// s.log.LogRequestError(reqID.(string), "Not found", http.StatusNotFound)
+	// 	http.Error(w, "Not found", http.StatusNotFound)
+	// 	return
+	// }
+	if r.Method != http.MethodGet {
+		// s.log.LogRequestError(reqID.(string), "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "../front/index.html")
+}
+
+func Run() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", serveHome)
+	// mux.HandleFunc("/ws", s.serveWS)
+
+	// withMW := s.log.LoggingMW(mux)
+
+	l, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	// Success handler for authenticated users
-	successHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		twitterUser, err := twitterLogin.UserFromContext(ctx)
-		if err != nil {
-			http.Error(w, "Failed to get Twitter user from context", http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(w, "Welcome, %s!", twitterUser.ScreenName)
-	})
+	// log.Info("server listening on port " + s.server.Addr)
 
-	// Failure handler for authentication errors
-	failureHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintln(w, "Twitter login failed.")
-	})
+	if err := http.Serve(l, mux); err != nil {
+		log.Fatal(err.Error())
+	}
+}
 
-	// Setup routes
-	http.Handle("/twitter/login", twitterLogin.LoginHandler(oauth1Config, nil))
-	http.Handle("/twitter/callback", twitterLogin.CallbackHandler(oauth1Config, successHandler, failureHandler))
+type Config struct {
+	XConsumerKey    string
+	XConsumerSecret string
+}
 
-	fmt.Println("Server listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func LoadConfig() (Config, error) {
+	err := godotenv.Load()
+
+	cfg := Config{
+		XConsumerKey:    os.Getenv("X_CONSUMER_KEY"),
+		XConsumerSecret: os.Getenv("X_CONSUMER_SECRET"),
+	}
+
+	return cfg, err
+}
+
+func main() {
+	cfg, err := LoadConfig()
+	fmt.Println(err)
+
+	fmt.Printf("%+v\n", cfg)
+
+	// OAuth1 Config for Twitter
+	// oauth1Config := &oauth1.Config{
+	// 	ConsumerKey:    ConsumerKey,
+	// 	ConsumerSecret: ConsumerSecret,
+	// 	CallbackURL:    "http://localhost:8080/twitter/callback",
+	// 	Endpoint:       oauth1.TwitterEndpoint,
+	// }
+
+	// // Success handler for authenticated users
+	// successHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	// 	ctx := req.Context()
+	// 	twitterUser, err := twitterLogin.UserFromContext(ctx)
+	// 	if err != nil {
+	// 		http.Error(w, "Failed to get Twitter user from context", http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	fmt.Fprintf(w, "Welcome, %s!", twitterUser.ScreenName)
+	// })
+
+	// // Failure handler for authentication errors
+	// failureHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	// 	fmt.Fprintln(w, "Twitter login failed.")
+	// })
+
+	// // Setup routes
+	// http.Handle("/twitter/login", twitterLogin.LoginHandler(oauth1Config, nil))
+	// http.Handle("/twitter/callback", twitterLogin.CallbackHandler(oauth1Config, successHandler, failureHandler))
+
+	// fmt.Println("Server listening on :8080")
+	// http.HandleFunc("/api", homeHandler)
+	http.HandleFunc("/", serveHome)
+
+	Run()
 }
